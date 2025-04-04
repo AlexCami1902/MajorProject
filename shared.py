625a56f228f75fb76610c4481feb117950c50a21
AlexCami1902 <alexiocam07@gmail.com>
2025-04-04 19:06:14 +1100

    Update shared.py
    Made changes after testing the program to ensure that the winner declaration says that the correct team won after the added coin toss feature. The old code just had the home team batting first, and as such, if the team batting first won, then it knew it was the home team. Now, by passing the variable through the shared.py file, it can tell who won even with the coin flip functionality.
    
    Signed-off-by: AlexCami1902 <alexiocam07@gmail.com>

--- shared.py
+++ shared.py
@@ -2,6 +2,8 @@
 import pygame
 home_team = ""
 away_team = ""
+first_batting_team = ""
+second_batting_team = ""
 match_location = ""
 home_team_colour = pygame.Color(0, 0, 0)
 away_team_colour = pygame.Color(0, 0, 0)
