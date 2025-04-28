5ae2fa7be7decc2d829f12552ec755cf5dc624e4
AlexCami1902 <alexiocam07@gmail.com>
2025-04-29 09:27:38 +1000

    Update shared.py
    Holiday updates
    
    Signed-off-by: AlexCami1902 <alexiocam07@gmail.com>

--- shared.py
+++ shared.py
@@ -1,9 +1,14 @@
-# shared.py
-import pygame
-home_team = ""
-away_team = ""
-first_batting_team = ""
-second_batting_team = ""
-match_location = ""
-home_team_colour = pygame.Color(0, 0, 0)
-away_team_colour = pygame.Color(0, 0, 0)
+import csv
+
+names = [
+        {"firstname": "Alex", "lastname": "Camilleri"},
+        {"firstname": "Kath", "lastname": "Camilleri"},
+        {"firstname": "Ben", "lastname": "Camilleri"},
+]
+
+with open('test.csv', mode='w') as csvfile:
+    fieldnames = names[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in names:
+        writer.writerow(row)
