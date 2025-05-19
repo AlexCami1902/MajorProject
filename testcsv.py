ccdbec1d45c600e1ad59c5b9242260fc73b690eb
AlexCami1902 <alexiocam07@gmail.com>
2025-05-19 17:32:06 +1000

    Update testcsv.py
    Signed-off-by: AlexCami1902 <alexiocam07@gmail.com>

--- testcsv.py
+++ testcsv.py
@@ -1,14 +1,1 @@
-import csv
 
-names = [
-        {"firstname": "Alex", "lastname": "Camilleri"},
-        {"firstname": "Kath", "lastname": "Camilleri"},
-        {"firstname": "Ben", "lastname": "Camilleri"},
-]
-
-with open('test.csv', mode='w') as csvfile:
-    fieldnames = names[0].keys()
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for row in names:
-        writer.writerow(row)
